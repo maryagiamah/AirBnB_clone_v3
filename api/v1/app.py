57e7c9d8dfe8ebe6dd5bf2b4de6390c1f2006ac1
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-11-22 15:32:14 +0100

    you

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,13 +3,14 @@
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
-def shutdown():
+def shutdown(self):
     """Close database"""
     storage.close()
 
