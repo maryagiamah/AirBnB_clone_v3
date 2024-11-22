46eff43a739956f0190171ab6b2681eeaf76e5aa
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-11-22 16:03:49 +0100

    you

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Creste A flask app"""
-from flask import Flask
+"""Create A flask app"""
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -13,6 +13,12 @@ app.register_blueprint(app_views)
 def shutdown(self):
     """Close database"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(err):
+    """404 Error hanlder"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
